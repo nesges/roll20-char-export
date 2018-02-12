ccfdbac09f941cc80e53ce249dcc0724e85e7ebc
nesges <nesges@gmail.com>
2018-02-12 18:34:33 +0100

    autogm-v0.3

--- autogm.js
+++ autogm.js
@@ -5,12 +5,12 @@
 //
 // Github:      https://github.com/nesges/roll20-char-export
 // by:          Thomas Nesges <thomas@nesges.eu>
-// Version:     0.2
+// Version:     0.3
 
 on('ready',function() {
     'use strict';
     
-    log('-=> autogm v0.2 <=-');
+    log('-=> autogm v0.3 <=-');
     
     // run setControlledBy() on startup
     setControlledBy();
@@ -30,47 +30,48 @@ on('ready',function() {
         
         characters.forEach(function(char, index) {
             var charName = char.get("name");
-            
-            // if controlledby and inplayerjournals are not set
-            if(char.get('controlledby')=='' && char.get('inplayerjournals')=='') {
-                var m;
-                var playerName;
-                // if charname ist assembled of text1::text2
-                if(m = charName.match(/(.+)\s*::\s*(.+)\s*/)) {
-                    // search for a player named text2
-                    playerName = m[2].trim();
-                // if charname ist assembled of text1 (text2)
-                } else if(m = charName.match(/(.+)\s*[(]\s*(.+)\s*[)]/)) {
-                    playerName = m[2].trim();
-                }
-                
-                if(typeof playerName != 'undefined') {
-                    var players = findObjs({
+            var playerName;
+            var player;
+            var m;
+            // if charname ist assembled of text1::text2
+            if(m = charName.match(/(.+)\s*::\s*(.+)/)) {
+                // search for a player named text2
+                playerName = m[2].trim();
+            }
+            if(typeof playerName != 'undefined') {
+                var players = findObjs({
                         _type: "player",
                         _displayname: playerName
                     });
-                    // if player found
-                    if(typeof players != 'undefined' && players.length==1) {
-                        // set controlledby and inplayerjournals to that player
-                        char.set('controlledby', players[0].id);
-                        char.set('inplayerjournals', players[0].id);
-                        logChat('<b>' + charName + '</b> controlled by <b>' + playerName + '</b> now');
-                    } else {
-                        // warn that there is no player named text2
-                        logChat('<b>' + charName + '</b>: player <b>' + playerName + '</b> not found');
-                    }
-
-                    // does the character have a token?
-                    var tokens = findObjs({
-                        _type: "graphic",
-                        represents: char.id
-                    });
-                    
-                    if(tokens.length < 1) {
-                        logChat('WARNING: <b>' + charName + '</b> has no token');
-                    }
+                // if player found
+                if(typeof players == 'undefined' || players.length!=1) {
+                    // playername is not legit
+                    logChat('<b>' + charName + '</b>: player <b>' + playerName + '</b> not found');
+                    return;
                 }
-            }           
+                player = players[0];
+            }
+            
+            //log(charName + ' c:' + char.get('controlledby') + ' j:' + char.get('inplayerjournals'))
+            
+            // if player found and
+            // if controlledby is not set or to an other user
+            if(typeof player != 'undefined' && (char.get('controlledby')=='' || char.get('controlledby')!=player.id)) {
+                // set controlledby and inplayerjournals to that player
+                char.set('controlledby', player.id);
+                char.set('inplayerjournals', player.id);
+                logChat('<b>' + charName + '</b> controlled by <b>' + playerName + '</b> now');
+
+                // does the character have a token?
+                var tokens = findObjs({
+                    _type: "graphic",
+                    represents: char.id
+                });
+                
+                if(tokens.length < 1) {
+                    logChat('WARNING: <b>' + charName + '</b> has no token');
+                }
+            }
         });
     }
     
